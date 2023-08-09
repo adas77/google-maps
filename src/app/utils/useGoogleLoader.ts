9536a107baf9c52b4fe0efe46643e8a56b246a14
Adam Pilewski <apilewski01@gmail.com>
2023-08-09 20:52:24 +0200

    ...

--- src/app/utils/useGoogleLoader.ts
+++ src/app/utils/useGoogleLoader.ts
@@ -1,13 +1,13 @@
 import { useJsApiLoader } from "@react-google-maps/api";
 
-const apiKey = process.env.API_KEY!;
+// const apiKey = process.env.API_KEY!;
 // if (!apiKey) {
 //   throw new Error("Add Google Maps API key");
 // }
 export const useGoogleLoader = () => {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: apiKey,
+    googleMapsApiKey: process.env.API_KEY!,
   });
   return { isLoaded };
 };
